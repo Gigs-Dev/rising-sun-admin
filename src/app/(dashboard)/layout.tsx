660947e3a8357bfd2a478d89bb5c9605162cf00c
Emmanuel Omejua <omejuaifeanyi2@mail.com>
2025-11-27 17:19:06 +0100

    implement mobile menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,14 @@
+import Navbar from '@/components/general/Navbar';
 import Sidebar from '@/components/general/Sidebar';
 import React from 'react'
 
 const DashboardLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <main className="flex gap-4 max-w-[1400px] w-full px-2 mx-auto">
+    <main className="flex gap-4 max-w-[1400px] w-full md:mx-auto min-h-screen h-full">
         <Sidebar/>
 
-        <section className="flex flex-col">
+        <section className="flex flex-col w-full h-full px-2">
+            <Navbar/>
             {children}
         </section>
     </main>

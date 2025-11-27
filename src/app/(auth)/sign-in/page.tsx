686541593110d2d986cad403b20b49e06dec5207
Emmanuel Omejua <omejuaifeanyi2@mail.com>
2025-11-27 08:07:28 +0100

    implement font set-up

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <main className='bg-inherit flex items-center justify-center h-screen'>
+        <div className="bg-black flex items-center justify-center">SignInPage</div>
+    </main>
+  )
+}
+
+export default SignInPage;
---
+++ src/assets/constants/font.config.ts
@@ -0,0 +1,47 @@
+import { Geist, Geist_Mono, Inter, Katibeh } from "next/font/google";
+
+import localFont from "next/font/local";
+
+export const atomiAge = localFont({
+  src: [
+    {
+      path: "../fonts/atomi-age/AtomiAge-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-atomic-age",
+});
+
+
+export const nunitoSans = localFont({
+  src: [
+    {
+      path: "../fonts/nunito-sans/NunitoSans-Italic-VariableFont_YTLC,opsz,wdth,wght.ttf",
+      weight: "300",
+      style: "Italic"
+    },
+    {
+      path: "../fonts/nunito-sans/NunitoSans-VariableFont_YTLC,opsz,wdth,wght.ttf",
+      weight: "400",
+      style: "normal"
+    },
+    {
+      path: "../fonts/nunito-sans/NunitoSans-VariableFont_YTLC,opsz,wdth,wght.ttf",
+      weight: "500",
+      style: "normal"
+    },
+  ],
+  variable: "--font-nunito-sans",
+})
+
+export const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin']
+})
+
+export const katibeh = Katibeh({
+  weight: ['400'],
+  subsets: ['latin'],
+  style: "normal"
+})
